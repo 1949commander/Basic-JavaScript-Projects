7e5d43ed1a671f8b402f58be0b8ccfe26f2ef5a8
Brian Reeves <1949commander@gmail.com>
2020-03-28 23:25:25 -0700

    Initial commit

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+var A = ("\“Our toaster is also confused." 
++ " It doesn’t know where bagels go.\"");
+var B = "A new challenge"
+var Family = "The Jones", Dad = "Joe", Mom = "Jane", Son = "Bill";
+var Family = Family.fontcolor("blue");
+var Dad = Dad.fontcolor("green");
+document.write(A);
+window.alert(B);
+document.write(Family) ;
+document.write(Mom) ;
+document.write(Dad) ;
+document.write(4 + 12);
