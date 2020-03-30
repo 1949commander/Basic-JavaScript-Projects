00556993f96cbbde7e3e0687b3302dcf5a755ed5
Brian Reeves <1949commander@gmail.com>
2020-03-30 09:14:26 -0700

    Function Assignments
    Upload of basic function assignments

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function My_function() {
+    var x = 5;
+    var y = 8;
+    var z = x * y;
+    document.getElementById("demo").innerHTML = z;
+}
+
+function newFunction() {
+    var sentence = "I need a break from";
+    sentence += " corona isolation!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
