5e176eeabf5e01d96ea880bdcff9550c2c8a7f9b
Brian Reeves <1949commander@gmail.com>
2020-04-03 11:04:38 -0700

    Assignment 5 Page 115
    Upload assignment 5 page 115 to master

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,69 @@
+function my_Dictionary() {
+    var Car = {
+        Brand:"Studebaker",
+        Model:"Commander",
+        Body:"Coupe",
+        Trim:"Regal",
+        Color:"Green",
+        Year:1949,
+    };
+  
+    document.getElementById("Dictionary").innerHTML = (typeof "Studebaker");
+}
+function my_Function() {
+    document.getElementById("Test").innerHTML = (-3E310);
+}
+function my_Function_2() {
+    document.getElementById("Test-2").innerHTML = (3E310);
+}
+function my_Function_3() {
+    document.getElementById("Test-3").innerHTML = (10 > 2);
+}
+function my_Function_4() {
+    document.getElementById("Test-4").innerHTML = (10 < 2);
+}
+function my_Function_5() {
+    console.log(5 + 6);
+}
+function my_Function_6() {
+    document.write ("10" + 5)
+}
+function my_Function_7() {
+    console.log(5 > 6);
+}
+function my_Function_8() {
+    document.write (10 == 5)
+}
+function my_Function_9() {
+    document.write (10 == 10)
+}
+function my_Function_10() {
+    document.getElementById("Test-10").innerHTML = (5 === 5);
+}
+function my_Function_11() {
+    document.getElementById("Test-11").innerHTML = (5 === "A");
+}
+function my_Function_12() {
+    document.getElementById("Test-12").innerHTML = (5 === "5");
+}
+function my_Function_13() {
+    document.getElementById("Test-13").innerHTML = (5 === 6);
+}
+function my_Function_14() {
+    document.getElementById("Test-14").innerHTML = (6 > 1 && 20 > 10);
+}
+function my_Function_15() {
+    document.getElementById("Test-15").innerHTML = (6 < 1 && 20 > 10);
+}
+function my_Function_16() {
+    document.getElementById("Test-16").innerHTML = (6 < 1 || 20 > 10);
+}
+function my_Function_17() {
+    document.getElementById("Test-17").innerHTML = (6 < 1 || 20 < 10);
+}
+function my_Function_18() {
+    document.getElementById("Test-18").innerHTML = !(20 > 10);
+}
+function my_Function_19() {
+    document.getElementById("Test-19").innerHTML = !(5 > 10);
+}
