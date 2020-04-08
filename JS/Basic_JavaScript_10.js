b4491a88937aa8e6249def4c4ed518a4794370ec
Brian Reeves <1949commander@gmail.com>
2020-04-08 11:59:00 -0700

    Assignment 10
    Script question

---
+++ JS/Basic_JavaScript_10.js
@@ -0,0 +1,55 @@
+/* function Call_Loop() {
+    var text = "";
+    var i = 0;
+    while (i <= 15) {
+        text += "<br> The number is " + i;
+        i++;
+    }
+    document.getElementById("Loop").innerHTML = text;
+}
+function Call_Char () {
+    var str = "Good Morning USA!";
+    var n = str.length;
+    document.getElementById("strng").innerHTML = n;
+}
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+        }
+        document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+function cat_pics() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
+        Cat_Picture[3] + ".";
+}
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "Red";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+    Musical_Instrument.type + " was " +Musical_Instrument.price +" color is " +Musical_Instrument.color+ " brand is " +Musical_Instrument.brand;
+}
+function test_Function() {
+    var X = 82;
+    document.write(X);
+    {
+        let X = 33;
+        document.write("<br>" + X);
+    }
+    document.write("<br>" + X)
+} */
+
+var x = myFunction(5, 3); 
+
+function myFunction(a, b) {
+  return a * b;   
+}  
+document.getElementById("demo").innerHTML = x;
